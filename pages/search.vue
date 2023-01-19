7bd491e94843e38879291da78248f72324685400
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2023-01-19 13:47:48 +0100

    add basics for search through recipes

--- pages/search.vue
+++ pages/search.vue
@@ -4,10 +4,10 @@
 
       <Combobox v-model="selectedRecipe">
         <ComboboxInput @change="query = $event.target.value" placeholder="Search meal name.." class="p-2 rounded-lg" />
-        <ComboboxOptions>
-          <ComboboxOption v-for="recipe in filteredRecipe" :key="recipe.id" :value="recipe">
+        <ComboboxOptions :key="componentKey">
+          <ComboboxOption v-for="recipe in filteredRecipes" :key="recipe.id" :value="recipe">
             <span class="cursor-pointer hover:text-accent-normal" @click="navigateTo(`/show-meal/${recipe.id}`)">{{
-              recipe.title
+              recipe.title || ''
             }}</span>
           </ComboboxOption>
         </ComboboxOptions>
@@ -20,7 +20,9 @@
           <ChevronDownIcon :class="[open && 'rotate-180 transform', 'w-6 h-6 inline']" />
         </DisclosureButton>
         <DisclosurePanel class="text-gray-500">
-          filter through categories etc
+
+          <button @click="searchCategories = !searchCategories">dont search through categories</button>
+          <div>{{ searchCategories? 'true': 'false' }}</div>
         </DisclosurePanel>
       </Disclosure>
     </section>
@@ -42,15 +44,25 @@ import {
   DisclosurePanel,
 } from '@headlessui/vue';
 
-const people = [
-  { id: 1, name: 'Durward Reynolds', unavailable: false },
-  { id: 2, name: 'Kenton Towne', unavailable: false },
-  { id: 3, name: 'Therese Wunsch', unavailable: false },
-  { id: 4, name: 'Benedict Kessler', unavailable: true },
-  { id: 5, name: 'Katelyn Rohan', unavailable: false },
-]
+
 
 const recipes = [
+  {
+    title: 'pizza',
+    id: 987533454647,
+    ingredients: [{ name: 'pizza', id: 3 }, { name: 'pancake', id: 1 }],
+    categories: [{ name: 'soup', id: 11 }, { name: 'slow cook', id: 7 }],
+    picture: false,
+    recipe: {
+      ingredients: [
+        { name: 'water', amount: '2' },
+        { name: 'chilli', amount: '150g' },
+        { name: 'spinach', amount: '1dl' },
+        { name: 'salt', amount: '2' },
+      ],
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+    }
+  },
   {
     title: 'pasta bolognese',
     id: 9873654274693,
@@ -69,9 +81,9 @@ const recipes = [
   },
   {
     title: 'fish & chips',
-    id: 98732765425674693,
+    id: 2765425674695,
     ingredients: [{ name: 'fish', id: 3 }, { name: 'potato', id: 1 }],
-    categories: [{ name: 'favourites', id: 11 }, { name: 'comfort food', id: 7 }],
+    categories: [{ name: 'fav', id: 11 }, { name: 'healty', id: 7 }],
     picture: false,
     recipe: {
       ingredients: [
@@ -85,66 +97,38 @@ const recipes = [
   }
 ];
 
-
-const selectedRecipe = ref(recipes[1])
 const query = ref('')
+const selectedRecipe = ref(recipes[1])
+const filteredRecipes = ref(recipes)
+const searchCategories = ref(true)
+const searchIngredients = ref(true)
+const componentKey = ref(0);
 
-const filteredRecipe = computed(() =>
-  query.value === ''
-    ? recipes
-    : recipes.filter((recipe) => {
-      return recipe.title.toLowerCase().includes(query.value.toLowerCase())
-    })
-)
+const filterSearch = () => {
+  if (query.value !== '') {
+
+    const afterFilter = [];
+
+    for (const recipe of recipes) {
+      let isMatch = false;
+
+      //find categories with query in category
+      const foundByCategories = recipe.categories.filter(i => i.name.includes(query.value));
+      if (foundByCategories.length > 0) isMatch = true;
+
+      const foundByIngredients = recipe.ingredients.filter(i => i.name.includes(query.value));
+      if (foundByIngredients.length > 0) isMatch = true;
+
+      if (recipe.title.includes(query.value)) isMatch = true;
+
+      //if one of them returns true, add to list. if none, remove from list
+      if (isMatch) afterFilter.push(recipe)
+
+
+      filteredRecipes.value = afterFilter;
+    }
+
+  }
+}
+watch(query, filterSearch)
 </script>
-
-// <!--
-
-<template>
-  // <article class="p-4 bg-white rounded-lg bg-opacity-10">
-    // <input type="text" placeholder="Search meal..">
-
-    // <section>
-      // Result
-      // </section>
-    // </article>
-  // </template>
-
-<script>
-// const recipe = [{
-//   title: 'pasta bolognese',
-//   id: 9873654274693,
-//   ingredients: [{ name: 'pasta', id: 3 }, { name: 'pork mince', id: 1 }],
-//   categories: [{ name: 'favourites', id: 11 }, { name: 'comfort food', id: 7 }],
-//   picture: false,
-//   recipe: {
-//     ingredients: [
-//       { name: 'eggs', amount: '2' },
-//       { name: 'butter', amount: '150g' },
-//       { name: 'tomato sauce', amount: '1dl' },
-//       { name: 'onions', amount: '2' },
-//     ],
-//     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
-//   }
-// },
-// {
-//   title: 'fish & chips',
-//   id: 98732765425674693,
-//   ingredients: [{ name: 'fish', id: 3 }, { name: 'potato', id: 1 }],
-//   categories: [{ name: 'favourites', id: 11 }, { name: 'comfort food', id: 7 }],
-//   picture: false,
-//   recipe: {
-//     ingredients: [
-//       { name: 'eggs', amount: '2' },
-//       { name: 'butter', amount: '150g' },
-//       { name: 'tomato sauce', amount: '1dl' },
-//       { name: 'onions', amount: '2' },
-//     ],
-//     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
-//   }
-// }]
-
-
-
-
-// </script> -->
