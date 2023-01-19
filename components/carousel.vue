74f1c2f04b0fd52e1c66f5c62ce4beaaef2e4cc5
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2023-01-19 21:26:29 +0100

    render carousel from user category list

--- components/carousel.vue
+++ components/carousel.vue
@@ -2,13 +2,13 @@
   <h4 class="ml-1 text-white font-semibold">{{ label }}</h4>
   <Carousel :items-to-scroll="1" :wrap-around="true" :settings="settings"
     class="flex flex-col justify-items-stretch mb-8">
-    <Slide v-for="slide of category" :key="slide.id" class="h-36 pb-2 px-1 cursor-pointer"
+    <Slide v-for="slide of filteredCategories" :key="slide.id" class="h-36 pb-2 px-1 cursor-pointer"
       @click="navigateTo(`/show-meal/${slide.id}`)">
       <article class="w-full h-full flex rounded-lg overflow-hidden drop-shadow-lg">
-        <img src="@/assets/img/bolognese.jpg" :alt="slide.name" class="grow object-cover " />
+        <img src="@/assets/img/bolognese.jpg" :alt="slide.title" class="grow object-cover " />
         <h6
           class="absolute flex items-center px-2 overflow-hidden break-all bottom-0 w-full text-xs font-light text-white h-[18%] bg-prime-normal bg-opacity-80">
-          {{ slide.name }}
+          {{ slide.title }}
         </h6>
       </article>
     </Slide>
@@ -25,17 +25,203 @@ const settings = {
 };
 
 interface IContent {
+  categoryId: number;
   name: string;
-  id: number;
   img?: string;
 }
 
 interface ICarousel {
-  category: IContent[],
+  category: IContent,
   label: string
 }
 
-defineProps<ICarousel>();
+const { category } = defineProps<ICarousel>();
+const filteredCategories = reactive([{ title: '', id: 0, picture: false }]);
+
+interface IUser {
+  userName: string,
+  userEmail: string,
+  password: string,
+  categories: [{ categoryId: number, name: string }],
+  recipes: [
+    {
+      title: string,
+      id: number,
+      keywords: [],
+      categories: [{ name: string, categoryId: number }]
+      picture: boolean,
+      recipe: [{ ingredients: [], description: string }]
+    }
+  ]
+}
+
+const user = {
+  userName: 'Kalle',
+  userEmail: 'kalle@gmail.com',
+  password: 'jf948yf980w34fh',
+  categories: [
+    { categoryId: 1, name: 'vegetarian' },
+    { categoryId: 2, name: 'vegan' },
+    { categoryId: 3, name: 'healthy' },
+    { categoryId: 4, name: 'spicy' },
+    { categoryId: 5, name: 'soup' },
+    { categoryId: 6, name: 'salad' },
+    { categoryId: 7, name: 'comfort food' },
+    { categoryId: 8, name: 'quick' },
+    { categoryId: 9, name: 'special' },
+    { categoryId: 10, name: 'slow cook' },
+    { categoryId: 11, name: 'favourites' }
+  ],
+  recipes: [
+    {
+      title: 'pizza',
+      id: 987533454647,
+      keywords: [{ name: 'pizza', keywordId: 3 }, { name: 'pancake', keywordId: 1 }],
+      categories: [{ name: 'soup', categoryId: 5 }, { name: 'slow cook', categoryId: 10 }],
+      picture: false,
+      recipe: {
+        ingredients: [
+          { name: 'water', amount: '2' },
+          { name: 'chilli', amount: '150g' },
+          { name: 'spinach', amount: '1dl' },
+          { name: 'salt', amount: '2' },
+        ],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+      }
+    },
+    {
+      title: 'pasta bolognese',
+      id: 9873654274693,
+      keywords: [{ name: 'pasta', keywordId: 3 }, { name: 'pork mince', keywordId: 1 }],
+      categories: [{ name: 'favourites', categoryId: 11 }, { name: 'comfort food', categoryId: 7 }],
+      picture: false,
+      recipe: {
+        ingredients: [
+          { name: 'eggs', amount: '2' },
+          { name: 'butter', amount: '150g' },
+          { name: 'tomato sauce', amount: '1dl' },
+          { name: 'onions', amount: '2' },
+        ],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+      }
+    },
+    {
+      title: 'fish & chips',
+      id: 2765425674695,
+      keywords: [{ name: 'fish', keywordId: 3 }, { name: 'potato', keywordId: 1 }],
+      categories: [{ name: 'favourites', categoryId: 11 }, { name: 'healty', categoryId: 3 }],
+      picture: false,
+      recipe: {
+        ingredients: [
+          { name: 'eggs', amount: '2' },
+          { name: 'butter', amount: '150g' },
+          { name: 'tomato sauce', amount: '1dl' },
+          { name: 'onions', amount: '2' },
+        ],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+      }
+    },
+    {
+      title: 'chicken',
+      id: 27232425674695,
+      keywords: [{ name: 'chicken', keywordId: 3 }, { name: 'potato', keywordId: 1 }],
+      categories: [{ name: 'vegetarian', categoryId: 1 }, { name: 'healty', categoryId: 3 }, { name: 'vegan', categoryId: 2 }],
+      picture: false,
+      recipe: {
+        ingredients: [
+          { name: 'eggs', amount: '2' },
+          { name: 'butter', amount: '150g' },
+          { name: 'tomato sauce', amount: '1dl' },
+          { name: 'onions', amount: '2' },
+        ],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+      }
+    },
+    {
+      title: 'curry',
+      id: 27232425674695,
+      keywords: [{ name: 'spicy', keywordId: 3 }, { name: 'chili', keywordId: 1 }],
+      categories: [{ name: 'vegetarian', categoryId: 1 }, { name: 'quick', categoryId: 8 }, { name: 'special', categoryId: 9 }],
+      picture: false,
+      recipe: {
+        ingredients: [
+          { name: 'eggs', amount: '2' },
+          { name: 'butter', amount: '150g' },
+          { name: 'tomato sauce', amount: '1dl' },
+          { name: 'onions', amount: '2' },
+        ],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+      }
+    },
+    {
+      title: 'Stirfry',
+      id: 27287654676,
+      keywords: [{ name: 'noodles', keywordId: 3 }, { name: 'potato', keywordId: 1 }],
+      categories: [{ name: 'vegetarian', categoryId: 1 }, { name: 'healty', categoryId: 3 }],
+      picture: false,
+      recipe: {
+        ingredients: [
+          { name: 'eggs', amount: '2' },
+          { name: 'butter', amount: '150g' },
+          { name: 'tomato sauce', amount: '1dl' },
+          { name: 'onions', amount: '2' },
+        ],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+      }
+    },
+    {
+      title: 'broccoli soup',
+      id: 2728548746,
+      keywords: [{ name: 'noodles', keywordId: 3 }, { name: 'potato', keywordId: 1 }],
+      categories: [{ name: 'vegan', categoryId: 2 }, { name: 'healty', categoryId: 3 }, { name: 'soup', categoryId: 5 }, { name: 'vegetarian', categoryId: 1 }],
+      picture: false,
+      recipe: {
+        ingredients: [
+          { name: 'eggs', amount: '2' },
+          { name: 'butter', amount: '150g' },
+          { name: 'tomato sauce', amount: '1dl' },
+          { name: 'onions', amount: '2' },
+        ],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+      }
+    },
+    {
+      title: 'pea soup',
+      id: 3527554889846,
+      keywords: [{ name: 'peas', keywordId: 3 }, { name: 'potato', keywordId: 1 }],
+      categories: [{ name: 'vegan', categoryId: 2 }, { name: 'healty', categoryId: 3 }, { name: 'soup', categoryId: 5 }, { name: 'vegetarian', categoryId: 1 }],
+      picture: false,
+      recipe: {
+        ingredients: [
+          { name: 'eggs', amount: '2' },
+          { name: 'butter', amount: '150g' },
+          { name: 'tomato sauce', amount: '1dl' },
+          { name: 'onions', amount: '2' },
+        ],
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'
+      }
+    }
+  ]
+};
+const setUser = ref(user.recipes)
+
+
+
+
+const filterCategories = () => {
+  const mealsInCategory = [];
+  for (const recipe of setUser.value) {
+    const found = recipe.categories.filter(arr => arr.categoryId === category.categoryId)
+    if (found.length > 0) mealsInCategory.push({ title: recipe.title, id: recipe.id, picture: recipe.picture })
+  }
+
+  Object.assign(filteredCategories, mealsInCategory);
+  console.log('filteredCat', filteredCategories);
+}
+
+filterCategories();
+
+
 
 </script>
 
--- pages/search.vue
+++ pages/search.vue
@@ -1,28 +1,60 @@
 <template>
-  <article class="p-4 bg-white rounded-lg bg-opacity-10 text-prime-normal">
-    <section>
+  <article class="p-2 mb-6 bg-white rounded-lg bg-opacity-10 text-prime-normal">
+    <h2 class="text-white">Search meal</h2>
+    <section class="">
+      <aside class="flex justify-between flex-nowrap mb-2">
+        <button @click="searchTitle = !searchTitle"
+          :class="[searchTitle ? 'bg-accent-normal' : 'bg-white bg-opacity-10', 'm-1 px-2 py-1 rounded-full text-xs drop-shadow-lg hover:text-white']">By
+          name</button>
+        <button @click="searchCategories = !searchCategories"
+          :class="[searchCategories ? 'bg-accent-normal' : 'bg-white bg-opacity-10', 'm-1 px-2 py-1 rounded-full text-xs drop-shadow-lg hover:text-white']"
+          first-letter:>By
+          category</button>
+        <button @click="searchIngredients = !searchIngredients"
+          :class="[searchIngredients ? 'bg-accent-normal' : 'bg-white bg-opacity-10', 'm-1 px-2 py-1 rounded-full text-xs drop-shadow-lg hover:text-white']">By
+          ingredient</button>
+      </aside>
+      <div>
+        <Combobox v-model="selectedRecipe">
+          <ComboboxInput @change="query = $event.target.value" placeholder="Search meal name.."
+            class="px-2 py-1 rounded-lg" />
+          <ComboboxOptions :key="componentKey">
+            <ComboboxOption v-for="recipe in filteredRecipes" :key="recipe.id" :value="recipe">
+              <div
+                class="cursor-pointer my-2 rounded-lg px-2 bg-white bg-opacity-10 text-white hover:text-accent-normal"
+                @click="navigateTo(`/show-meal/${recipe.id}`)">{{
+                  recipe.title || ''
+                }}</div>
+            </ComboboxOption>
+          </ComboboxOptions>
+        </Combobox>
+      </div>
 
-      <Combobox v-model="selectedRecipe">
-        <ComboboxInput @change="query = $event.target.value" placeholder="Search meal name.." class="p-2 rounded-lg" />
-        <ComboboxOptions :key="componentKey">
-          <ComboboxOption v-for="recipe in filteredRecipes" :key="recipe.id" :value="recipe">
-            <span class="cursor-pointer hover:text-accent-normal" @click="navigateTo(`/show-meal/${recipe.id}`)">{{
-              recipe.title || ''
-            }}</span>
-          </ComboboxOption>
-        </ComboboxOptions>
-      </Combobox>
+
     </section>
+
+  </article>
+
+  <article class="p-4 bg-white rounded-lg bg-opacity-10 text-prime-normal">
+
     <section>
       <Disclosure v-slot="{ open }">
         <DisclosureButton class="py-2">
-          Filter search
-          <ChevronDownIcon :class="[open && 'rotate-180 transform', 'w-6 h-6 inline']" />
+          <h2 class="text-white inline">Generate suggestions</h2>
+          <ChevronDownIcon :class="[open && 'rotate-180 transform', 'w-6 h-6 inline text-white']" />
         </DisclosureButton>
         <DisclosurePanel class="text-gray-500">
-
-          <button @click="searchCategories = !searchCategories">dont search through categories</button>
-          <div>{{ searchCategories? 'true': 'false' }}</div>
+          <label for="no-of-meals">Number of meal</label>
+          <select id="no-of-meals">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+          </select>
+          from categories
         </DisclosurePanel>
       </Disclosure>
     </section>
@@ -43,6 +75,7 @@ import {
   DisclosureButton,
   DisclosurePanel,
 } from '@headlessui/vue';
+import { DiagnosticCategory } from 'typescript';
 
 
 
@@ -102,33 +135,52 @@ const selectedRecipe = ref(recipes[1])
 const filteredRecipes = ref(recipes)
 const searchCategories = ref(true)
 const searchIngredients = ref(true)
+const searchTitle = ref(true)
 const componentKey = ref(0);
 
 const filterSearch = () => {
   if (query.value !== '') {
 
-    const afterFilter = [];
+    const filtered = [];
 
     for (const recipe of recipes) {
       let isMatch = false;
 
-      //find categories with query in category
-      const foundByCategories = recipe.categories.filter(i => i.name.includes(query.value));
-      if (foundByCategories.length > 0) isMatch = true;
+      if (searchCategories.value) {
+        console.log('recipe in serach', recipe);
+        const foundByCategories = recipe.categories.filter(i => i.name.includes(query.value));
+        if (foundByCategories.length > 0) isMatch = true;
+      }
 
-      const foundByIngredients = recipe.ingredients.filter(i => i.name.includes(query.value));
-      if (foundByIngredients.length > 0) isMatch = true;
+      if (searchIngredients.value) {
+        const foundByIngredients = recipe.ingredients.filter(i => i.name.includes(query.value));
+        if (foundByIngredients.length > 0) isMatch = true;
+      }
 
-      if (recipe.title.includes(query.value)) isMatch = true;
+      if (searchTitle.value) {
+        if (recipe.title.includes(query.value)) isMatch = true;
+      }
 
       //if one of them returns true, add to list. if none, remove from list
-      if (isMatch) afterFilter.push(recipe)
-
-
-      filteredRecipes.value = afterFilter;
+      if (isMatch) filtered.push(recipe)
+      filteredRecipes.value = filtered;
     }
-
   }
 }
+
+
+//generate meals:  until length is 3 (number of meals) for each 2 recipe
+const meals = 7;
+const categories = 2;
+const result = meals % categories;
+console.log('result', result);
+// for (let i = 0; i < 3; i++) {
+//   //find 
+// }
+//get length of Category
+// find random index from length
+//divide meals/categories = number from each
+
+
 watch(query, filterSearch)
 </script>
