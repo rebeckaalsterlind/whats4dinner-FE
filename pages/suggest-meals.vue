9b8954d9d4b05458a689cf1ca32781e065db142f
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2023-01-30 13:10:40 +0100

    remove selected suggestion and generate new + delete sel categories

--- pages/suggest-meals.vue
+++ pages/suggest-meals.vue
@@ -2,15 +2,20 @@
   <article class="p-4 bg-white rounded-lg bg-opacity-10">
 
     <h2 class="text-accent-normal text-center w-full">Generate meal suggestions</h2>
+    <div v-for="cat in savedCategories">{{ cat.name }}</div>
     <section v-if="tooFewMeals">There were only {{ showSuggestions.length }} meals in selected categories</section>
     <section v-if="showSuggestions.length > 0" v-for="meal in showSuggestions">
       <Pill :label="meal.title" />
-      <XMarkIcon class="text-white w-4 h-4" @click="removeSuggested(meal)" />
+      <XMarkIcon class="text-white w-4 h-4" @click="deleteMeal(meal)" />
     </section>
+    <button @click="generateMeals(false, savedCategories)">Generate {{ selectedNumber -
+    showSuggestions.length }} new meals</button>
     <section class="flex gap-4 items-center my-4 justify-between">
       <div>{{ selectedNumber }} meals</div>
-      <div>from {{ selectedCategories.map((category) => category.name).join(', ') }}</div>
-      <Button label="Generate" @click="generateMeals" />
+      <!-- <div>from {{ selectedCategories.map((category: { name: string; }) => category.name).join(', ') }}</div> -->
+      <Button label="Generate" @click="generateMeals(true, savedCategories)" />
+      <Button label="Reset categories" @click="resetCategories" />
+
     </section>
     <section class=" flex flex-col justify-between items-start text-whitew-full">
 
@@ -27,10 +32,10 @@
       </Listbox>
 
       <Listbox v-model="selectedCategories" multiple>
-        <!-- <ListboxLabel> {{ selectedCategories.map((category) => category.name).join(', ') }} </ListboxLabel> -->
         <ListboxButton class="text-start content-fit">Select categories</ListboxButton>
         <ListboxOptions>
           <ListboxOption v-for="category in userCategories" :key="category.categoryId" :value="category"
+            @click="handleCategories(category)"
             class="cursor-pointer h-6 bg-white bg-opacity-10 my-1 px-2 rounded-lg text-prime-normal hover:text-accent-normal">
             {{ category.name }}
           </ListboxOption>
@@ -45,7 +50,6 @@
 <script setup lang="ts">
 import {
   Listbox,
-  ListboxLabel,
   ListboxButton,
   ListboxOptions,
   ListboxOption,
@@ -83,50 +87,69 @@ interface IRec {
   };
 }[]
 
-// const { recipes } = defineProps<IUser>()
 const selectedCategories = ref([] as ICategory[]);
-const selectedNumber = ref(0);
+const savedCategories = ref([] as ICategory[]);
+const selectedNumber = ref();
 const showSuggestions = reactive([] as IRec[]);
 const tooFewMeals = ref(false);
-const searchThroughRecipes = ref(userRecipes.value)
-const generateMeals = () => {
-  tooFewMeals.value = false;
-  const generatedMeals = [] as IRec[];
+const remainingRecipes = ref(userRecipes.value)
+const componentKey = ref(0)
+const generateMeals = (reset: boolean, categories: ICategory[]) => {
 
-  //random sort category list. 
-  const shuffledCategories = [];
-  const arrayToShuffle = [...selectedCategories.value];
-  while (arrayToShuffle.length) {
-    shuffledCategories.push(arrayToShuffle.splice(~~(Math.random() * arrayToShuffle.length), 1)[0]);
+  console.log('categoreis', categories.length);
+  if (categories.length <= 0) {
+    console.log('choose categories');
   }
+  else if (!selectedNumber.value) {
+    console.log('selected number', selectedNumber.value);
+  } else {
 
-  for (const selectedCategory of shuffledCategories) {
-    for (const recipe of searchThroughRecipes.value) {
-      if (generatedMeals.length < selectedNumber.value) {
-        const matchingCategory = recipe.categories.find((category: { categoryId: number; }) => selectedCategory.categoryId === category.categoryId)
-        if (matchingCategory) {
-          const findDouble = generatedMeals.find((savedRecipe: { id: number; }) => savedRecipe.id === recipe.id)
-          if (!findDouble) generatedMeals.push(recipe);
+
+
+    tooFewMeals.value = false;
+    const generatedMeals = [] as IRec[];
+
+    let recipes;
+    let num;
+    if (reset) {
+      recipes = userRecipes.value;
+      num = selectedNumber.value
+    } else {
+      recipes = remainingRecipes.value
+      num = selectedNumber.value - showSuggestions.length;
+    }
+
+    for (const selectedCategory of shuffle(categories)) {
+      for (const recipe of recipes) {
+        if (generatedMeals.length < num) {
+          const matchingCategory = recipe.categories.find((category: { categoryId: number; }) => category.categoryId === selectedCategory.categoryId)
+          if (matchingCategory) {
+            const findDouble = generatedMeals.find((savedRecipe: { id: number; }) => savedRecipe.id === recipe.id)
+            if (!findDouble) generatedMeals.push(recipe)
+          }
         }
       }
     }
+
+    if (generatedMeals.length < num) {
+      tooFewMeals.value = true;
+    }
+
+    let newRound = generatedMeals;
+    if (!reset) newRound = [...showSuggestions, ...generatedMeals]
+
+    Object.assign(showSuggestions, newRound);
   }
 
-  if (generatedMeals.length < selectedNumber.value) {
-    tooFewMeals.value = true;
-  }
-  Object.assign(showSuggestions, generatedMeals)
 }
 
-
-const removeSuggested = (meal: any) => {
-
-  console.log('searchthgoughrecipes', searchThroughRecipes.value);
+//fix any
+const deleteMeal = (meal: any) => {
 
   //remove from list of recipies to generate from
-  for (const i of showSuggestions) {
-    const index = searchThroughRecipes.value.findIndex(recipes => recipes.id === i.id)
-    searchThroughRecipes.value.splice(index, 1);
+  for (const suggestion of showSuggestions) {
+    const index = remainingRecipes.value.findIndex(recipes => recipes.id === suggestion.id)
+    remainingRecipes.value.splice(index, 1);
   }
 
   //remove from selected
@@ -135,6 +158,30 @@ const removeSuggested = (meal: any) => {
 
 }
 
-//add button for generate all new or just the remaining
+const shuffle = (categories: ICategory[]) => {
+  const shuffledCategories = [];
+  //need this?
+  const arrayToShuffle = [...categories];
+  while (arrayToShuffle.length) {
+    shuffledCategories.push(arrayToShuffle.splice(~~(Math.random() * arrayToShuffle.length), 1)[0]);
+  }
+  return shuffledCategories
+}
 
+//save selected categories
+const handleCategories = (category: ICategory) => {
+  const pushOrSplice = savedCategories.value.findIndex(cat => cat.categoryId === category.categoryId)
+  if (pushOrSplice !== -1) savedCategories.value.splice(pushOrSplice, 1)
+  else savedCategories.value.push(category)
+}
+
+const resetCategories = () => {
+  savedCategories.value = [];
+  console.log('saved caat', savedCategories.value);
+  componentKey.value += 1;
+}
 </script>
+
+
+
+
